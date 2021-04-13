da78e5b6dc69d4470492e1d868a61bfde89eacae
Aydrian Howard <aydrian@gmail.com>
2021-04-13 11:22:59 -0400

    Added base twitch webhook function with signature verification

--- netlify/functions/twitch.js
+++ netlify/functions/twitch.js
@@ -1,6 +1,36 @@
-exports.handler = async function (event, context) {
+import withVerifyTwitch from "../lib/withVerifyTwitch";
+
+async function twitchHandler(event, context) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      headers: { Allow: "POST" },
+      body: "Method Not Allowed"
+    };
+  }
+
+  const body = JSON.parse(event.body);
+  const messageType = event.headers["twitch-eventsub-message-type"];
+  if (messageType === "webhook_callback_verification") {
+    return {
+      statusCode: 200,
+      body: body.challenge
+    };
+  } else if (messageType === "notification") {
+    const {
+      event,
+      subscription: { type }
+    } = body;
+
+    console.log(
+      `Receiving ${type} request for ${event.broadcaster_user_name}: `,
+      event
+    );
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" })
+    statusCode: 200
   };
-};
+}
+
+export const handler = withVerifyTwitch(twitchHandler);
